19ec41a401e1f28651dff58cd696b83621933cbd
Killian Raoux <killian@semifir.com>
2021-09-09 14:34:46 +0200

    !feat(routing): ajout d'un systeme de routing

--- src/components/header/HeaderItem.jsx
+++ src/components/header/HeaderItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const HeaderItem = (props) => {
     console.log(props)
     const style = {
         backgroundColor: props.bgColor
     }
-    return <div className="header-item" style={style}>
-        {props.titre}
-    </div>
+    return <Link to={props.link}>
+        <div className="header-item" style={style}>
+            {props.titre}
+        </div>
+    </Link>
 }
