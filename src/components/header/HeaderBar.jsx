dc7caeb04279bab251b7bd84db94ff2961d7938c
Killian Raoux <killian@semifir.com>
2021-09-09 14:45:17 +0200

    feat(routing): ajout d'un nouveau bouton pour la navigation vers le formulaire des articles

--- src/components/header/HeaderBar.jsx
+++ src/components/header/HeaderBar.jsx
@@ -4,8 +4,9 @@ import { HeaderItem } from './HeaderItem'
 
 export const HeaderBar = () => {
     return <div className="header-bar">
-        <HeaderItem titre="Home 1" bgColor="green" link="/" />
-        <HeaderItem titre="Infos" bgColor="blue" link="/articles" />
+        <HeaderItem titre="Home 1" bgColor="lightgray" link="/" />
+        <HeaderItem titre="Liste de nos articles" bgColor="lightgray" link="/articles" />
+        <HeaderItem titre="Créer un nouvel article" bgColor="lightgray" link="/articles/new" />
     </div>
 }
 
